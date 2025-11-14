c1f6e9345187d90b6a9e587bafca26eeb8b0cff6
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-11-14 23:01:07 +0530

    signin page

--- Module-2/Day_5/index1.js
+++ Module-2/Day_5/index1.js
@@ -7,5 +7,18 @@ function signup(userName){
             users.push(userName)
                 return "Signup Successful, please Login"
             }
-        }
-        console.log(signup("Nani"))
+}
+    console.log(signup("Nani"))
+
+function login(userName,password){
+    let users=["Smita","Rohan","Jaanu","Harsha"]
+    if(!users.includes(userName)){
+        return "User Not found, Please Signin"
+    }
+    if(password=="Emp@123"){
+        return "Login Successful"
+    }
+    else{
+        return "Wrong Password"
+    }
+}
