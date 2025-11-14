d952ade2b1f7b7c55d5a7683adea282996cd90a7
Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-11-14 16:55:44 +0530

    Update index.js

--- Module-2/Day_5/index.js
+++ Module-2/Day_5/index.js
@@ -2,7 +2,7 @@ function prime(num){
     if (num<=1){
         return false
     }
-    for(let i=2;i<=Math.surt(num);i++){
+    for(let i=2;i<=Math.sqrt(num);i++){
         if(num % i==0){
             return false
         }
