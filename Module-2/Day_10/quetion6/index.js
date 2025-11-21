29758847ef10ad575ec57fadff04593fa1d28f48
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-11-21 13:46:24 +0530

    this is q6 adjs

---
+++ Module-2/Day_10/quetion6/index.js
@@ -0,0 +1,13 @@
+// const isEven=n=>n%2==0
+// console.log(isEven(5))
+// console.log(isEven(4))
+
+
+// result=marks>=35?"Pass":"Fail"
+
+
+const greet=name=>{
+    console.log(`Hello,${name ?name:"Guest"}`)
+}
+greet("yhfuf")
+greet()
