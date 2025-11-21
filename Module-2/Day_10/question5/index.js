2713fe8cd1f5204c0a572c1f3cf94b7fda1224af
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-11-21 13:17:55 +0530

    this is q5 adjs

---
+++ Module-2/Day_10/question5/index.js
@@ -0,0 +1,18 @@
+// const username="Vinay";
+// const course="Java Script"
+// console.log(`Hello ${username},welcome to the ${course} course`)
+
+
+
+// const name="Sam";
+// const age=21;
+// const student={
+//     name,
+//     age,
+//     greet(){
+//         console.log("Hello")
+//     }
+// }
+
+
+const getFullName=(first,last)=>`${first},${last}`
