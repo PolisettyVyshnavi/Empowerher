5e5a908f736940400825186144692e625e4645f4
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-11-25 15:18:17 +0530

    2nd commit

--- Module-2/Day_12/question2/index.js
+++ Module-2/Day_12/question2/index.js
@@ -14,3 +14,47 @@ const counter = createCounter();
 counter.increment(); 
 counter.increment();
 counter.decrement(); 
+
+
+function createBankAccount(){
+    let balance=0
+    let transactionHistory=[]
+    return{
+        deposit:function(amount){
+            if(amount>0){
+                balance=balance+amount
+                transactionHistory.push(`Deposited:${amount}`)
+                console.log(`Deposited:${amount}`)
+            }
+            else{
+                console.log("Deposite amount must be positive number")
+            }
+        },
+        withdraw:function(amount){
+            if(amount>0&& amount<=balance){
+                balance=balance-amount
+                transactionHistory.push(`Withdraw${amount}`)
+                console.log(`Withdraw${amount}`)
+            }
+            else if(amount>balance){
+                transactionHistory.push(`Failed withdraw:${amount} Insufficient`)
+                console.log(`Insufficient balance`)
+            }
+            else{
+                console.log("Withdraw amount must be positive number")
+            }
+        },
+        checkBalance:function(){
+            console.log(`Current Balance${balance}`)
+            return balance
+        },
+        getTransactionHistory:function(){
+            return[...transactionHistory]
+        }
+    }
+}
+const account = createBankAccount();
+account.deposit(500); 
+account.withdraw(200); 
+account.withdraw(400); 
+console.log(account.balance); 
