cb80e9e44ea1b44dc44715dca6cb9968ba9bbd94
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-12-23 15:22:53 +0530

    Context API

---
+++ Module-2/Day_26/question2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Component1 from "./components/Component1";
+
+function App() {
+  return (
+    <div>
+      <h2>Context API Demo</h2>
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
