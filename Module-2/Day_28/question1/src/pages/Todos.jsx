6ca2896d755b3628995f067a780fbf2a7dbb89b9
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-12-25 13:36:37 +0530

    ReactRouting

--- Module-2/Day_28/question1/src/pages/Todos.jsx
+++ Module-2/Day_28/question1/src/pages/Todos.jsx
@@ -1,22 +1,17 @@
-// pages/Todos.js
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 const Todos = () => {
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
-
-  useEffect(() => {
+    useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
       .then(res => res.json())
       .then(data => setTodos(data));
   }, []);
-
   const handleLogout = () => {
     localStorage.removeItem('isLoggedIn');
     navigate('/login');
   };
-
   return (
     <div>
       <h2>Todos</h2>
@@ -25,7 +20,7 @@ const Todos = () => {
         {todos.map(todo => (
           <li key={todo.id}>
             <Link to={`/todos/${todo.id}`}>
-              <strong>{todo.title}</strong> - {todo.completed ? '✅ Completed' : '❌ Not Completed'}
+              <strong>{todo.title}</strong> - {todo.completed ? ' Completed' : ' Not Completed'}
             </Link>
           </li>
         ))}
