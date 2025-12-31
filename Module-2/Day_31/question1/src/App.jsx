5435485ee6b49adb5e09fb39d748e1acacca22ac
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2025-12-31 18:17:29 +0530

    Lazy Component

--- Module-2/Day_31/question1/src/App.jsx
+++ Module-2/Day_31/question1/src/App.jsx
@@ -2,19 +2,15 @@ import React, { useState, Suspense, lazy } from 'react';
 const HeavyComponent = lazy(() => import('./HeavyComponent'));
 function App() {
   const [counter, setCounter] = useState(0);
-
   return (
     <div>
       <h1>React.memo & Lazy Loading</h1>
       <p>Counter: {counter}</p>
       <button onClick={() => setCounter(c => c + 1)}>Increment Counter</button>
-
-      {/* ✅ Lazy load with fallback */}
       <Suspense fallback={<p>Loading heavy component...</p>}>
         <HeavyComponent />
       </Suspense>
     </div>
   );
 }
-
 export default App;
