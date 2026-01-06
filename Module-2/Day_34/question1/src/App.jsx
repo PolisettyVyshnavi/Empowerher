dda07d6cccce5703c8445780037db05811665cec
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-06 18:06:33 +0530

    Tailwind

--- Module-2/Day_34/question1/src/App.jsx
+++ Module-2/Day_34/question1/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import FeedbackForm from "./components/FeedbackForm.jsx";
 import ImageSlideshow from "./components/ImageSlideshow.jsx";
 import TodoList from "./components/TodoList.jsx";
-
 function App() {
   return (
     <div className="space-y-10 p-6">
@@ -12,5 +11,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
