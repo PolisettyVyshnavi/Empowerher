28a9f3653d978bb680febd38ee54ab5d744d2701
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-06 18:02:48 +0530

    Tailwind

--- Module-2/Day_34/question1/src/components/ui/card.jsx
+++ Module-2/Day_34/question1/src/components/ui/card.jsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import { cn } from "../../lib/utils";
-
 export function Card({ className, ...props }) {
   return (
     <div className={cn("rounded-lg border bg-white shadow-sm", className)} {...props} />
   );
 }
-
 export function CardHeader({ className, ...props }) {
   return <div className={cn("p-4 font-semibold text-lg", className)} {...props} />;
 }
-
 export function CardContent({ className, ...props }) {
   return <div className={cn("p-4", className)} {...props} />;
 }
-
 export function CardFooter({ className, ...props }) {
   return <div className={cn("p-4 border-t", className)} {...props} />;
 }
