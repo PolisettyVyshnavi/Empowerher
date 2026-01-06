a36403d4baf065e986f06f5e288f708fcdcc59bb
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-06 18:05:53 +0530

    Tailwind

--- Module-2/Day_34/question1/src/components/ImageSlideshow.jsx
+++ Module-2/Day_34/question1/src/components/ImageSlideshow.jsx
@@ -1,19 +1,15 @@
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { Button } from "./ui/button";
 import { useState } from "react";
-
 const images = [
   "https://wallpapers.com/images/hd/pawan-kalyan-red-and-black-v78r04hfwy78pv9o.jpg",
   "https://tse2.mm.bing.net/th/id/OIP.UZj9CIvsUPepaOrixvxSqQHaHq?rs=1&pid=ImgDetMain&o=7&rm=3",
   "https://blogtobollywood.com/wp-content/uploads/2017/05/Prabhas-4.jpg",
 ];
-
 export default function ImageSlider() {
   const [index, setIndex] = useState(0);
-
   const next = () => setIndex((index + 1) % images.length);
   const prev = () => setIndex((index - 1 + images.length) % images.length);
-
   return (
     <Card className="max-w-md mx-auto mt-10">
       <CardContent className="flex justify-center">
