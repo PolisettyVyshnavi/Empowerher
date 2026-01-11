8ba396e4cdb24229a2e6899e24cd9b1acf3e5527
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-11 18:29:49 +0530

    es module

--- Module-4/Day2/question2/index.js
+++ Module-4/Day2/question2/index.js
@@ -1,10 +1,5 @@
-// index.js
-
-// Import the function from math.js
 import { checkPrime } from './math.js';
-
-// Test the function with different values
-console.log("Is 2 prime? ->", checkPrime(2));   // true
+console.log("Is 2 prime? ->", checkPrime(2));  
 console.log("Is 4 prime? ->", checkPrime(4));   // false
 console.log("Is 17 prime? ->", checkPrime(17)); // true
 console.log("Is 20 prime? ->", checkPrime(20)); // false
