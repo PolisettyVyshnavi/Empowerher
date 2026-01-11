607c538be9ceec96657fb75fb1ad76091708d396
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-11 18:30:04 +0530

    es module

--- Module-4/Day2/question2/index.js
+++ Module-4/Day2/question2/index.js
@@ -1,6 +1,6 @@
 import { checkPrime } from './math.js';
 console.log("Is 2 prime? ->", checkPrime(2));  
-console.log("Is 4 prime? ->", checkPrime(4));   // false
-console.log("Is 17 prime? ->", checkPrime(17)); // true
-console.log("Is 20 prime? ->", checkPrime(20)); // false
-console.log("Is 23 prime? ->", checkPrime(23)); // true
+console.log("Is 4 prime? ->", checkPrime(4));  
+console.log("Is 17 prime? ->", checkPrime(17)); 
+console.log("Is 20 prime? ->", checkPrime(20)); 
+console.log("Is 23 prime? ->", checkPrime(23)); 
