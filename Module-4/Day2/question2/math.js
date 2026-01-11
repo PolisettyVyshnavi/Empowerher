2d6d1be2c064c873a79882f1adf8ccd2ec2288d2
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-11 18:30:24 +0530

    es module

--- Module-4/Day2/question2/math.js
+++ Module-4/Day2/question2/math.js
@@ -1,12 +1,7 @@
-// math.js
-
-// Function to check if a number is prime
 export function checkPrime(num) {
-  if (num <= 1) return false; // 0 and 1 are not prime
-  if (num === 2) return true; // 2 is prime
-  if (num % 2 === 0) return false; // even numbers > 2 are not prime
-
-  // Check divisibility up to sqrt(num)
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false; 
   for (let i = 3; i <= Math.sqrt(num); i += 2) {
     if (num % i === 0) return false;
   }
