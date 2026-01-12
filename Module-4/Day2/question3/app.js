3df3ef107c56001bf4cb4b2e7d9345d46b795ba7
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-12 15:27:20 +0530

    os & fs

--- Module-4/Day2/question3/app.js
+++ Module-4/Day2/question3/app.js
@@ -8,21 +8,21 @@ console.log("Total CPU cores:", os.cpus().length);
 async function fileOperations() {
   try {
     await fs.writeFile('data.txt', 'Hello World');
-    console.log("✅ data.txt created");
+    console.log("data.txt created");
 
     await fs.writeFile('Readme.md', '## This is first line in Readme');
-    console.log("✅ Readme.md created");
+    console.log("Readme.md created");
 
     const data = await fs.readFile('data.txt', 'utf-8');
-    console.log("📖 Content of data.txt:", data);
+    console.log("Content of data.txt:", data);
 
     await fs.appendFile('data.txt', '\nThis is second line');
-    console.log("✅ Appended to data.txt");
+    console.log(" Appended to data.txt");
 
     await fs.unlink('Readme.md');
-    console.log("🗑️ Readme.md deleted");
+    console.log("Readme.md deleted");
   } catch (err) {
-    console.error("❌ Error:", err.message);
+    console.error("Error:", err.message);
   }
 }
 fileOperations();
