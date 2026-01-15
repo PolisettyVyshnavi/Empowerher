d6289bd5c2a2b69eca87232babd436cc44ab69b0
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-15 15:17:32 +0530

    Express App

--- Module-4/Day4/question2/project/index.js
+++ Module-4/Day4/question2/project/index.js
@@ -1,4 +1,3 @@
-// index.js
 import express from 'express';
 import os from 'os';
 import dns from 'dns';
@@ -6,13 +5,9 @@ import { readFileContent } from './read.js';
 
 const app = express();
 const PORT = 3000;
-
-// Route: /test
 app.get('/test', (req, res) => {
   res.send("Test route is working!");
 });
-
-// Route: /readfile
 app.get('/readfile', async (req, res) => {
   try {
     const content = await readFileContent();
@@ -21,14 +16,11 @@ app.get('/readfile', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-
-// Route: /systemdetails
 app.get('/systemdetails', (req, res) => {
   const totalMemGB = (os.totalmem() / (1024 ** 3)).toFixed(2) + " GB";
   const freeMemGB = (os.freemem() / (1024 ** 3)).toFixed(2) + " GB";
   const cpuModel = os.cpus()[0].model;
   const cpuCores = os.cpus().length;
-
   res.json({
     platform: os.platform(),
     totalMemory: totalMemGB,
@@ -37,8 +29,6 @@ app.get('/systemdetails', (req, res) => {
     cpuCores: cpuCores
   });
 });
-
-// Route: /getip
 app.get('/getip', (req, res) => {
   dns.lookup('masaischool.com', { all: true }, (err, addresses) => {
     if (err) {
@@ -50,13 +40,9 @@ app.get('/getip', (req, res) => {
     });
   });
 });
-
-// Optional: Root route to avoid "Cannot GET /"
 app.get('/', (req, res) => {
   res.send("Welcome to the Express server!");
 });
-
-// Start server
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
