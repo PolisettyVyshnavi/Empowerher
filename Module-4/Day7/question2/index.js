b714b43f07ad3917802f7a999a22a1ac6e39afeb
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-20 20:16:50 +0530

    Express + Multer

--- Module-4/Day7/question2/index.js
+++ Module-4/Day7/question2/index.js
@@ -5,12 +5,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to the User Signup API!");
 });
-// Parse JSON for non-multipart routes
 app.use(express.json());
-
-// Mount router
 app.use("/users", usersRouter);
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
