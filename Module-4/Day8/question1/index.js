eb898c8cd2fee45273a621ee41a53a2b87b55045
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-21 13:10:45 +0530

    Todo Application

--- Module-4/Day8/question1/index.js
+++ Module-4/Day8/question1/index.js
@@ -3,9 +3,7 @@ import todosRouter from "./routes/todos.routes.js";
 
 const app = express();
 app.use(express.json());
-
 app.use("/todos", todosRouter);
-
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
--- Module-4/Day8/question1/routes/todos.routes.js
+++ Module-4/Day8/question1/routes/todos.routes.js
@@ -8,11 +8,9 @@ import {
 } from "../controllers/todos.controller.js";
 
 const router = express.Router();
-
 router.get("/", fetchTodos);
 router.get("/:todoId", fetchTodoById);
 router.post("/add", createTodo);
 router.put("/update/:todoId", editTodo);
 router.delete("/delete/:todoId", removeTodo);
-
 export default router;
