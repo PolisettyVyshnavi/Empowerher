a5e0266fe8a3e714c126c67109b3f2dc6db6f6fe
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-30 16:59:44 +0530

    user-todo  management

--- Module-4/Day14/src/middleware/validateUser.js
+++ Module-4/Day14/src/middleware/validateUser.js
@@ -1,8 +1,10 @@
 export default (req, res, next) => {
   const { name, email, password } = req.body;
+  
   if (!name || !email || !password) {
     return res.status(400).json({ error: "Name, email, and password are required" });
   }
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     return res.status(400).json({ error: "Invalid email format" });
