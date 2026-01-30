df1e0f32a713d66abf292459e12a1ddcbadd5180
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-01-30 17:00:45 +0530

    user-todo  management

--- Module-4/Day14/src/routes/todoRoutes.js
+++ Module-4/Day14/src/routes/todoRoutes.js
@@ -3,10 +3,8 @@ import { addTodo, getUserTodos, updateTodo, deleteTodo } from '../controllers/to
 import validateTodo from '../middleware/validateTodo.js';
 
 const router = express.Router();
-
 router.post('/add-todo', validateTodo, addTodo);
 router.get('/get-my-todo/:userId', getUserTodos);
 router.put('/update-todo/:todoId', updateTodo);
 router.delete('/delete-todo/:todoId', deleteTodo);
-
 export default router;
--- Module-4/Day14/src/routes/userRoutes.js
+++ Module-4/Day14/src/routes/userRoutes.js
@@ -3,8 +3,6 @@ import { signup, deleteUser } from '../controllers/userController.js';
 import validateUser from '../middleware/validateUser.js';
 
 const router = express.Router();
-
 router.post('/signup', validateUser, signup);
 router.delete('/:id', deleteUser);
-
 export default router;
