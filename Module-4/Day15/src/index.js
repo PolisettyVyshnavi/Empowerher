49a5b44d2a4298542d785497999d185583fbde15
Polisetty Vyshnavi <vyshnavipolisetty2004@gmail.com>
2026-02-03 14:17:54 +0530

    Mini User Authentication System

--- Module-4/Day15/src/index.js
+++ Module-4/Day15/src/index.js
@@ -2,14 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
-
 dotenv.config();
-
 const app = express();
 app.use(express.json());
-
 app.use('/api', authRoutes);
 app.use('/api', profileRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Auth API running on port ${PORT}`));
